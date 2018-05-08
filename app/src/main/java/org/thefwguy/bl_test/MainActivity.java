2521b2f6fa63b88ae7cb7215501c8ae76bd5bf9f
Stefano Bodini <stefano.bodini@elyxor.com>
2018-05-08 08:53:06 -0500

    Moved bluetooth management in a separate file

--- app/src/main/java/org/thefwguy/bl_test/MainActivity.java
+++ app/src/main/java/org/thefwguy/bl_test/MainActivity.java
@@ -1,16 +1,11 @@
 package org.thefwguy.bl_test;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,29 +17,11 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.reflect.Method;
-import java.util.Set;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "TFG MainActivity";
 
-    BluetoothAdapter mBluetoothAdapter;
-    BluetoothSocket mmSocket = null;
-    BluetoothDevice mmDevice = null;
-    OutputStream mmOutputStream = null;
-    InputStream mmInputStream = null;
-
-    Thread workerThread;
-    byte[] readBuffer;
-    int readBufferPosition;
-    int counter;
-    volatile boolean stopWorker;
-    // Device to pair
-    String bluetooth_device = "BLtest";     // String updated from settings
-    UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+    protected BluetoothManager bManager = new BluetoothManager();
 
     // connectionState represent the status of the Bluetooth connection
     // 0 -> disconnected
@@ -79,8 +56,7 @@ public class MainActivity extends AppCompatActivity {
         // Will eb enabled ONLY when connected to a device
         btn_send.setEnabled(false);
 
-
-        if (!findBT()) {
+        if (!bManager.findBT()) {
             // BL does not exist - disable Connect and Send buttons
             my_label.setText("Bluetooth not present");
             btn_connect.setEnabled(false);
@@ -97,18 +73,25 @@ public class MainActivity extends AppCompatActivity {
                 if (isChecked) {
                     // The toggle is enabled
                     if (connectionState == 0) {
-                        if (searchSlaveBT() == true) {
+                        btn_connect.setTextOff("Connecting");
+
+                        if (bManager.searchSlaveBT() == true) {
                             my_label.setText("Bluetooth Device Found");
                             connectionState = 1;
+
                             try {
-                                if (openBT() ) {
+                                if (bManager.openBT() ) {
+                                    my_label.setText("Bluetooth Opened");
                                     connectionState = 2;
                                     // Enable the Send button
                                     btn_send.setEnabled(true);
                                 } else {
-                                    discoverBT();
-                                    btn_connect.setTextOff("Abort");
+                                    bManager.discoverBT();
+
+                                    btn_connect.setTextOff("Connect");
                                     btn_connect.toggle();
+                                    connectionState=0;
+                                    my_label.setText("Bluetooth connection failed");
                                 }
                             } catch (IOException e) {
                                 e.printStackTrace();
@@ -121,7 +104,8 @@ public class MainActivity extends AppCompatActivity {
                     // The toggle is disabled
 //                    if (connectionState == 2) {
                         try {
-                            closeBT();
+                            bManager.closeBT();
+                            my_label.setText("Bluetooth Closed");
                             // Disable the Send button
                             btn_send.setEnabled(false);
 
@@ -140,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
                 // a Bluetooth connection exists - no need to further check.
                 try {
                      String msg = messageId.getText().toString();
-                     sendDataBT(msg);
+                     bManager.sendDataBT(msg);
                 } catch (IOException e) {
                      e.printStackTrace();
                 }
@@ -160,8 +144,8 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPref =
                 PreferenceManager.getDefaultSharedPreferences(this);
 
-        bluetooth_device = sharedPref.getString (SettingsActivity.KEY_PREF_DEVICE_NAME, "No setting");
-        idValue.setText(bluetooth_device);
+        bManager.bluetooth_device = sharedPref.getString (SettingsActivity.KEY_PREF_DEVICE_NAME, "No setting");
+        idValue.setText(bManager.bluetooth_device);
     }
 
     @Override
@@ -187,182 +171,4 @@ public class MainActivity extends AppCompatActivity {
 
         return  super.onOptionsItemSelected(item);
     }
-
-    // ---------------- Bluetooth functions -----------------------------------------
-
-    boolean findBT() {
-        Log.d(TAG, "findBT");
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            return false;
-        }
-        return true;
-    }
-
-    boolean discoverBT() {
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if (mBluetoothAdapter.startDiscovery()) {
-
-            //If discovery has started, then display the following toast....//
-            Toast.makeText(getApplicationContext(), "Discovering other bluetooth devices...",
-                    Toast.LENGTH_SHORT).show();
-        } else {
-
-            //If discovery hasn’t started, then display this alternative toast//
-            Toast.makeText(getApplicationContext(), "Something went wrong! Discovery has failed to start.",
-                    Toast.LENGTH_SHORT).show();
-        }
-        return true;
-    }
-
-    boolean searchSlaveBT() {
-        Log.d(TAG, "searchSlaveBT");
-        if(!mBluetoothAdapter.isEnabled())
-        {
-            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBluetooth, 0);
-        }
-
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        if(pairedDevices.size() > 0)
-        {
-            for(BluetoothDevice device : pairedDevices)
-            {
-                if(device.getName().equals(bluetooth_device))
-                {
-                    mmDevice = device;
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
-        try {
-            final Method  m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
-            return (BluetoothSocket) m.invoke(device, uuid);
-        } catch (Exception e) {
-            Log.e(TAG, "Could not create Insecure RFComm Connection",e);
-            return  device.createRfcommSocketToServiceRecord(uuid);
-        }
-    }
-
-    boolean openBT() throws IOException
-    {
-        Log.d(TAG, "openBT");
-
-        my_label.setText("Bluetooth Waiting connection");
-
-        try {
-            mmSocket = createBluetoothSocket(mmDevice);
-        } catch (IOException e1) {
-
-        }
-
-//        try {
-//            mmSocket = mmDevice.createInsecureRfcommSocketToServiceRecord(uuid);
-//        } catch (IOException e) {
-//            Log.d(TAG, "createInsecureRfcomm - " + e.getMessage());
-//        }
-
-        mBluetoothAdapter.cancelDiscovery();
-
-        try {
-            mmSocket.connect();     // Waiting here ?
-        } catch (IOException e) {
-            Log.d(TAG, "connect - " + e.getMessage());
-            e.printStackTrace();
-            my_label.setText("Bluetooth abort connection");
-            return false;
-        }
-
-        mmOutputStream = mmSocket.getOutputStream();
-        mmInputStream = mmSocket.getInputStream();
-
-        my_label.setText("Bluetooth Opened");
-
-        beginListenForData();
-        return true;
-    }
-
-    void beginListenForData()
-    {
-        Log.d(TAG, "beginListenForData");
-        final Handler handler = new Handler();
-        final byte delimiter = 10; //This is the ASCII code for a newline character
-
-        stopWorker = false;
-        readBufferPosition = 0;
-        readBuffer = new byte[1024];
-
-        workerThread = new Thread(new Runnable()
-        {
-            public void run()
-            {
-                while(!Thread.currentThread().isInterrupted() && !stopWorker)
-                {
-                    try
-                    {
-                        int bytesAvailable = mmInputStream.available();
-                        if(bytesAvailable > 0)
-                        {
-                            byte[] packetBytes = new byte[bytesAvailable];
-                            mmInputStream.read(packetBytes);
-                            for(int i=0;i<bytesAvailable;i++)
-                            {
-                                byte b = packetBytes[i];
-                                if(b == delimiter)
-                                {
-                                    byte[] encodedBytes = new byte[readBufferPosition];
-                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-                                    final String data = new String(encodedBytes, "US-ASCII");
-                                    readBufferPosition = 0;
-
-                                    handler.post(new Runnable()
-                                    {
-                                        public void run()
-                                        {
-                                            my_label.setText(data);
-                                        }
-                                    });
-                                }
-                                else
-                                {
-                                    readBuffer[readBufferPosition++] = b;
-                                }
-                            }
-                        }
-                    }
-                    catch (IOException ex)
-                    {
-                        stopWorker = true;
-                    }
-                }
-            }
-        });
-
-        workerThread.start();
-    }
-
-    void sendDataBT(String msg) throws IOException
-    {
-        Log.d(TAG, "sendDataBT");
-        msg += "\n";
-        mmOutputStream.write(msg.getBytes());
-        my_label.setText("Data Sent");
-    }
-
-    void closeBT() throws IOException
-    {
-        Log.d(TAG, "closeBT");
-        stopWorker = true;
-        if (mmOutputStream != null) mmOutputStream.close();
-        if (mmInputStream != null) mmInputStream.close();
-        if (mmSocket != null) mmSocket.close();
-        my_label.setText("Bluetooth Closed");
-    }
-
-
 }
