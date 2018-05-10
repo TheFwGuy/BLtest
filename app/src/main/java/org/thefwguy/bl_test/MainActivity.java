e02dd1d634da5b906cc63e8d21a59e88b4810c30
Stefano Bodini <stefano.bodini@elyxor.com>
2018-05-10 09:18:00 -0500

    Removed unused button Connect improvements

--- app/src/main/java/org/thefwguy/bl_test/MainActivity.java
+++ app/src/main/java/org/thefwguy/bl_test/MainActivity.java
@@ -111,6 +111,9 @@ public class MainActivity extends AppCompatActivity {
                             }
                         } else {
                             my_label.setText("Bluetooth Device NOT Found");
+                            btn_connect.setTextOff("Connect");
+                            btn_connect.toggle();
+                            connectionState=0;
                         }
                     }
                 } else {
@@ -145,14 +148,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
         SharedPreferences sharedPref =
